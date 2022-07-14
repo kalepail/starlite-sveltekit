f744c71d88bb55cf1e26363e502cc72be04f5f63
Tyler van der Hoeven <hi@tyvdh.com>
2022-07-14 11:36:24 -0400

    added github link and title

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,8 +2,14 @@
   import "../app.css";
 </script>
 
+<svelte:head>
+  <title>Starlite</title>
+</svelte:head>
+
 <main class="p-2 flex">
   <div class="flex flex-col" style:margin="0 auto">
     <slot />
   </div>
+
+  <iframe class="fixed bottom-2 left-2" src="https://ghbtns.com/github-btn.html?user=tyvdh&repo=starlite-sveltekit&type=star&count=true&size=large" frameborder="0" scrolling="0" width="170" height="30" title="GitHub"></iframe>
 </main>
